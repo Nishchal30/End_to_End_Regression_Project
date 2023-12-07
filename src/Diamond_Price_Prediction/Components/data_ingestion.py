03f64ed07ac0bf1fa3c46c62a852210b176381d0
Nishchal Jinturkar <nishchaljinturkar30@gmail.com>
2023-12-06 21:16:39 -0800

    updated the src folder

--- src/Diamond_Price_Prediction/Components/data_ingestion.py
+++ src/Diamond_Price_Prediction/Components/data_ingestion.py
@@ -1,4 +1,11 @@
-
+import pandas as pd
+import numpy as np
+from src.Diamond_Price_Prediction.logger import logging
+from src.Diamond_Price_Prediction.exception import CustomExecption
+from sklearn.model_selection import train_test_split
+from dataclasses import dataclass
+from pathlib import Path
+import os
 
 
 class DataIngestion:
@@ -6,4 +13,16 @@ class DataIngestion:
         pass
 
     def initiate_data_ingestion(self):
-        pass
+        logging.info("Data ingestion started here")
+
+        try:
+            data = pd.read_csv(Path(os.path.join("notebooks/data", "train.csv")))
+            logging.info("Read the training data")
+
+            train_data, test_data = train_test_split(data, test_size=0.3)
+            logging.info("train test split completed")
+
+            
+
+        except Exception as e:
+            pass
